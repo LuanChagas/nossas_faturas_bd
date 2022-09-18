14490388af17eefdd54ef2832ac36c2b7f7f654f
LuanChagas <luanchagas@hotmail.com>
2022-09-17 22:24:28 -0300

    adicionados campos descricao e vaor na tabela faturas_compras

--- CREATE/06_criando_fatura_compras.sql
+++ CREATE/06_criando_fatura_compras.sql
@@ -2,6 +2,8 @@ create table IF NOT EXISTS  fatura_compra(
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 id_fatura INT NOT NULL,
 id_compra INT NOT NULL,
+descricao varchar(50);
+valor DOUBLE NOT NULL ,
 FOREIGN KEY(id_fatura) REFERENCES faturas(id),
 FOREIGN KEY(id_compra) REFERENCES compras(id)
 );
