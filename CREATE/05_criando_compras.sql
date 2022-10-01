7cf48987020b390715fd3a9266a23f9cef8d8ca7
LuanChagas <luanchagas@hotmail.com>
2022-10-01 13:36:48 -0300

    correções

--- CREATE/05_criando_compras.sql
+++ CREATE/05_criando_compras.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS compras (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(50) NOT NULL,
-    descricao VARCHAR(50),
+    descricao VARCHAR(50) NOT NULL,
+    local VARCHAR(30),
     data_compra DATE NOT NULL,
-    valor DOUBLE NOT NULL,
-    parcelas TINYINT(2) default 1,
+    valor DECIMAL(5,2) NOT NULL,
+    parcelas TINYINT default 1,
     id_pessoa INT NOT NULL,
     id_cartao INT NOT NULL,
-    FOREIGN KEY(id_pessoa) REFERENCES pessoas(id),
-    FOREIGN KEY(id_cartao) REFERENCES cartao(id)
+    FOREIGN KEY(id_usuario) REFERENCES usuarios(id),
+    FOREIGN KEY(id_cartao) REFERENCES cartoes(id)
 );
